4ae77053e096293666a911f6b458cdaec355d115
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-06 00:47:52 +0100

    created convulation class

---
+++ src/python/image_processing/convulation_operation.py
@@ -0,0 +1,23 @@
+"""module for convulation opearation"""
+
+import numpy as np
+
+class Convulation:
+    
+    def __init__(self, input_matrix: np.ndarray, kernel: np.ndarray) -> None:
+        """__init__ method for Convulation Class"""
+
+        self.input_matrix = input_matrix
+        self.kernel = kernel
+
+    def conv_nd(self) -> np.ndarray:
+        """convulation method for 1D and 2D arrays(matrices)"""
+
+        pass
+    
+
+
+if __name__ == "__main__":
+    
+    input = np.array()
+    kernel = np.array()

d90e995c90a869e199cd3596a53daa794f79092a
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-09 23:56:38 +0100

    added path and completed display method

--- src/python/image_processing/display_image.py
+++ src/python/image_processing/display_image.py
@@ -11,5 +11,9 @@ def display_image(image_path: str, window_name: str) -> None:
         cv2.imshow(window_name, image)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-        cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    image_path = r"/home/hrithik-dev/ds-labs/image-processing-notebooks/notebooks/DATA/00-puppy.jpg"
+    display_image(image_path=image_path, window_name='puppy')
     

e21be1d8f34c6f28ce0132701796e3a3e9dd2cb3
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-20 01:35:53 +0100

    class skeleton

--- src/python/image_processing/image_interpolation.py
+++ src/python/image_processing/image_interpolation.py
@@ -1,1 +1,19 @@
 """module having nearest neighbour and bilinear interpolation"""
+
+from dataclasses import dataclass
+
+@dataclass
+class ImageInterPolation():
+    """class for image interpolation"""
+
+    pass
+
+def nearest_neighbour_interpolation(self) -> float:
+    """calculates intensity value using KNN method"""
+
+    pass
+
+
+def bilinear_interpolation(self) -> float:
+    
+    pass

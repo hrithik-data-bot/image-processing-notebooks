a102740d4115a5e6dd8ed48afd3fe693a2c3f0c8
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-10-27 00:45:16 +0000

    added values of coordinates and intensity values

--- src/python/image_processing/image_interpolation.py
+++ src/python/image_processing/image_interpolation.py
@@ -1,10 +1,17 @@
 """module having nearest neighbour and bilinear interpolation"""
 
+from typing import Tuple
 from dataclasses import dataclass
 
 @dataclass
 class ImageInterPolation():
-    """class for image interpolation"""
+    """class for image interpolation 2X2"""
+
+    coordinate_value_11: Tuple[Tuple, float] # ((x1, y1), Q11) 
+    coordinate_value_21: Tuple[Tuple, float] # ((x2, y1), Q21)
+    coordinate_value_12: Tuple[Tuple, float] # ((x1, y2), Q12)
+    coordinate_value_22: Tuple[Tuple, float] # ((x2, y2), Q22)
+    
 
     def nearest_neighbour_interpolation(self) -> float:
         """calculates intensity value using KNN method"""

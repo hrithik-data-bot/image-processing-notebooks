3bc2dfca1f0ecca42e3ed79d5a9c262be9e65119
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-11-05 19:35:39 +0000

    completed gaussian filter

--- src/python/image_processing/gaussian_filter.py
+++ src/python/image_processing/gaussian_filter.py
@@ -1,6 +1,7 @@
 """module to implement gaussian filter"""
 
 import math
+import numpy as np
 
 def gaussian_formula(x: int, y: int, sigma: float = 1) -> float:
     """gaussian formula function"""
@@ -15,5 +16,8 @@ if __name__ == "__main__":
                  (1, 0), (-1, 1), (0, 1), (1, 1)]
     gaussian_values = [gaussian_formula(pixel_i_j[0], pixel_i_j[1]) for pixel_i_j in my_values]
     normalised = [round(value/sum(gaussian_values), 4) for value in gaussian_values]
-    print(normalised)
+    normalised = np.array(normalised).reshape(3,3)
+    image_patch = np.array([[10, 20, 30], [20, 40, 20], [30, 20, 10]])
+    new_value = round(sum((normalised*image_patch).reshape(-1,1).flatten()))
+    print(new_value)
     

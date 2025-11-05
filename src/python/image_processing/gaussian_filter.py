774baa733c6cb4eaacaab3874e0b3ad601dfcf46
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-11-05 19:06:18 +0000

    complete method

--- src/python/image_processing/gaussian_filter.py
+++ src/python/image_processing/gaussian_filter.py
@@ -7,4 +7,6 @@ def gaussian_formula(x: int, y: int, sigma: float = 1) -> float:
 
     coeff_1 = 1/(2*math.pi*(sigma**2))
     coeff_2 = math.exp(-(x**2 + y**2)/(2*sigma**2))
-    
+    gaussian = coeff_1*coeff_2
+    return gaussian
+
